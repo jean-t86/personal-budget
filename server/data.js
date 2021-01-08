ee88a59d603707830ee3829e90e94153610d6ca6
J.Tadebois <jpm.tadebois@gmail.com>
2021-01-08 22:24:37 +0400

    Add starting envelopes data

---
+++ server/data.js
@@ -0,0 +1,57 @@
+const envelopes = [
+  {
+    id: 1,
+    category: 'Groceries',
+    limit: 400,
+  },
+  {
+    id: 2,
+    category: 'Gas',
+    limit: 100,
+  },
+  {
+    id: 3,
+    category: 'Health',
+    limit: 150,
+  },
+  {
+    id: 4,
+    category: 'Grooming',
+    limit: 200,
+  },
+  {
+    id: 5,
+    category: 'Clothing',
+    limit: 100,
+  },
+  {
+    id: 6,
+    category: 'Dining out',
+    limit: 200,
+  },
+  {
+    id: 7,
+    category: 'Household items',
+    limit: 100,
+  },
+  {
+    id: 8,
+    category: 'Pet care',
+    limit: 150,
+  },
+  {
+    id: 9,
+    category: 'Children\'s items',
+    limit: 100,
+  },
+  {
+    id: 10,
+    category: 'Savings',
+    limit: 125,
+  },
+  {
+    id: 11,
+    category: 'Rainy day funds',
+    limit: 100,
+  },
+];
